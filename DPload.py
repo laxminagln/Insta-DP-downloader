34502be35a10490918da0462d995854053c3b56a
nagln <laxminagln@gmail.com>
2020-08-25 19:44:23 +0530

    Update DPload.py

--- DPload.py
+++ DPload.py
@@ -1,11 +1,23 @@
-from selenium import webdriver
+from selenium import webdriver                                  #import libraries
+import urllib.request
 
-driver = webdriver.Chrome("path to web driver")
+driver = webdriver.Chrome("path to web driver")                 #give path to webdriiver
 
-url = "https://www.instagram.com/"
+url = "https://www.instagram.com/"                              
 url_user = input("enter the handle:")
-url_p = url + url_user
+url_p = url + url_user                                          #url of instagram user
 
-driver.get(url_p)
+driver.get(url_p)                                               #go to that webpage
 
-driver.find_element_by_xpath("//*img[@class='']")
+try:
+	image=driver.find_element_by_xpath('//img[@class="_6q-tv"]')  #finds the profile pic src
+except:
+	image=driver.find_element_by_xpath('//img[@class="be6sR"]')
+  
+img_link=image.get_attribute('src')                             #gets the source
+
+path="download path"                                            #path where you want to download
+
+urllib.request.urlretrieve(img_link,path)                       #retrieves the image
+
+print("The profile pic has been downloaded at: "+path)          #prints the confirmation message
